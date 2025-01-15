386f0ab29150cd2bcc8f7e98362177a1d886db6e
iSpookyLemon <kevinkli405@gmail.com>
2025-01-15 01:04:25 -0500

    update flask server

--- backend/app.py
+++ backend/app.py
@@ -43,6 +43,6 @@ def render_video(input_path, subtitles):
         return temp_output_file.name
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #from waitress import serve
-    #serve(app, host="0.0.0.0", port=8080)
+    #app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
